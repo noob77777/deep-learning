bd9e3921228f50cd69c7fd5fab0650a0d0f9e0de
noob77777 <abhishekpal7@outlook.com>
2020-08-12 17:37:43 +0530

    Fixed: Merged Conflicts

--- loss/loss.h
+++ loss/loss.h
@@ -7,6 +7,8 @@
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/export.hpp>
 
+const float ZERO = 0.0000000001;
+
 class LossFunction {
 	friend class boost::serialization::access;
 
@@ -35,14 +37,14 @@ class BinaryCrossEntropyLoss: public LossFunction {
 		float res = 0;
 		for(int i = 0; i < A.n; i++) {
 			for(int j = 0; j < A.m; j++) {
-				res += -(Y[i][j] * log(A[i][j]) + (1 - Y[i][j]) * log(1 - A[i][j]));
+				res += -(Y[i][j] * log(max(A[i][j], ZERO)) + (1 - Y[i][j]) * log(max(1 - A[i][j], ZERO)));
 			}
 		}
 		return res / A.m;
 	}
 
 	Matrix derivative(Matrix A, Matrix Y) {
-		Matrix res = ((Y/A) - ((Y-1)/(A-1))) * -1.0 / A.m;
+		Matrix res = ((Y/(A + ZERO)) - ((Y-1)/(A - 1 + ZERO))) * -1.0 / A.m;
 		return res;
 	}
 };
