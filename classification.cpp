8c457f91b1a982482f931ec972c3cba6e9946952
noob77777 <abhishekpal7@outlook.com>
2020-08-14 01:51:07 +0530

    Added: multi-class classification

--- classification.cpp
+++ classification.cpp
@@ -7,14 +7,14 @@
 #define LEARNING_RATE 0.01
 #define LAMBDA 0.1
 
-#define BATCH_ITERATION 100
+#define BATCH_ITERATION 50
 
 using namespace std;
 
 int main() {
 	int M = 1000;
 	Matrix X = Matrix(2, M, 'u');
-	Matrix Y = Matrix(3, M);
+	Matrix Y = Matrix(4, M);
 
 	for(int i = 0; i < X.m; i++) {
 		if (X[0][i] < 0.5 && X[1][i] < 0.5) {
@@ -23,14 +23,20 @@ int main() {
 		else if (X[0][i] >= 0.5 && X[1][i] >= 0.5) {
 			Y[1][i] = 1;
 		}
-		else {
+		else if (X[0][i] < 0.5 && X[1][i] >= 0.5) {
 			Y[2][i] = 1;
+		}
+		else {
+			Y[3][i] = 1;
 		}
 	}
 	
 
 	NeuralNetwork nn = NeuralNetwork();
-	nn.add_layer(new SoftmaxLayer(2, 3, LEARNING_RATE, LAMBDA));
+	nn.add_layer(new ReluLayer(2, 16, LEARNING_RATE, LAMBDA));
+	nn.add_layer(new ReluLayer(16, 8, LEARNING_RATE, LAMBDA));
+	nn.add_layer(new ReluLayer(8, 8, LEARNING_RATE, LAMBDA));
+	nn.add_layer(new SoftmaxLayer(8, 4, LEARNING_RATE, LAMBDA));
 	nn.add_loss_function(new SoftmaxCrossEntropyLoss());
 
 	for(int i = 0; i < 5; i++) {
@@ -46,7 +52,7 @@ int main() {
 		int max_idx2 = -1;
 		float nax1 = -1;
 		float nax2 = -1;
-		for(int j = 0; j < 3; j++) {
+		for(int j = 0; j < 4; j++) {
 			if (Y_pred[j][i] > nax1) {
 				nax1 = Y_pred[j][i];
 				max_idx1 = j;
